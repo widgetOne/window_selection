7900014858aec2ee04564bff2f2f25ecbe3c789f
Brian Coulter <pittsburgh137@gmail.com>
2017-09-24 12:07:27 -0400

    adds more test to 1-threshold func
    refactors the canned test data into a function
    that returns a dictionary, so that new tests can be
    added as permutations from the main input set in
    a consise manor. Then added a number of tests
    exploring each input parameter.

--- test__window_extraction.py
+++ test__window_extraction.py
@@ -10,12 +10,24 @@ def get_swing_data():
     return batting_data
 
 
-test_series = pandas.Series([0, 1, 1, 0, 1, 5, 1, 0, 1, 2, 0, 1, 1, 0])
-default_test_input = {'data': test_series, 'indexBegin': 3, 'indexEnd': 12, 'threshold': 0.5, 'winLength': 2}
-@pytest.mark.parametrize("input_dict,expected", [
-    (default_test_input, 4),
+test_series = pandas.Series([0, 1, 1, 0, 1, 5, 1, 0, 1, 2, 0, 2, 2, 0])
+def input_args(data=test_series, indexBegin=3, indexEnd=9, threshold=0.5, winLength=2):
+    return {'data': data, 'indexBegin': indexBegin, 'indexEnd': indexEnd,
+            'threshold': threshold, 'winLength': winLength}
+
+
+@pytest.mark.parametrize('test_name,input_dict,expected', [
+    ('basic test', input_args(), 4),
+    ('earlier start', input_args(indexBegin=0), 1),
+    ('one data point', input_args(indexBegin=0, indexEnd=0), None),
+    ('higher threshold', input_args(threshold=1.5, indexEnd=12), 11),
+    ('single point at indexbegin', input_args(winLength=1, indexBegin=2), 2),
+    ('single point after indexbegin', input_args(winLength=1, indexBegin=0), 1),
+    ('larger window', input_args(winLength=3, indexBegin=0), 4),
+    ('2 points w window of 3', input_args(winLength=3, indexBegin=0, indexEnd=1, threshold=-1), None),
 ])
-def test_searchContinuityAboveValue(input_dict, expected):
+def test_searchContinuityAboveValue(test_name, input_dict, expected):
+    print(test_name)
     result = searchContinuityAboveValue(**input_dict)
     assert result == expected
 
