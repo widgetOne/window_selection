98539ea5dc17072b80c5d0441ba1b0662b0f147b
Brian Coulter <pittsburgh137@gmail.com>
2017-09-24 16:41:54 -0400

    adding tests for all functions
    Adds tests for the two remaining
    function. For backSearchContinuityWithinRange, I simply
    used the same test set as for
    searchMultiContinuityWithinRange, which is now
    a standalone structure. I then added tests for
    test_searchContinuityAboveValueTwoSignals using the same
    basic layout as the two threshold tests.

--- test__window_extraction.py
+++ test__window_extraction.py
@@ -11,6 +11,9 @@ def get_swing_data():
 
 
 test_series = pandas.Series([0, 1, 1, 0, 1, 5, 1, 0, 1, 2, 0, 2, 2, 0])
+neg_test_series = test_series * -1
+
+
 def input_args_1t(data=test_series, indexBegin=3, indexEnd=9, threshold=0.5, winLength=2):
     return {'data': data, 'indexBegin': indexBegin, 'indexEnd': indexEnd,
             'threshold': threshold, 'winLength': winLength}
@@ -39,8 +42,8 @@ def input_args_mw(data=test_series, indexBegin=3, indexEnd=9, thresholdLo=0.5, t
     return {'data': data, 'indexBegin': indexBegin, 'indexEnd': indexEnd,
             'thresholdLo': thresholdLo, 'thresholdHi': thresholdHi, 'winLength': winLength}
 
-
-@pytest.mark.parametrize('test_name,input_dict,expected', [
+# creating a shared test set for backSearchContinuityWithinRange and searchMultiContinuityWithinRange
+high_and_low_threshold_test_set = [
     ('basic test', input_args_mw(), [(8, 9)]),
     ('higher threshold', input_args_mw(thresholdHi=5.1), [(4, 6), (8, 9)]),
     ('at higher threshold', input_args_mw(thresholdHi=5.0), [(8, 9)]),
@@ -51,7 +54,48 @@ def input_args_mw(data=test_series, indexBegin=3, indexEnd=9, thresholdLo=0.5, t
     ('lower threshold over upper', input_args_mw(thresholdLo=5), []),
     ('ending on part of window', input_args_mw(indexEnd=11), [(8, 9)]),
     ('starting on partial window', input_args_mw(indexBegin=2), [(8, 9)]),
+]
+
+
+@pytest.mark.parametrize('test_name,input_dict,expected', high_and_low_threshold_test_set)
+def test_backSearchContinuityWithinRange(test_name, input_dict, expected):
+    print(test_name)
+    result = backSearchContinuityWithinRange(**input_dict)
+    if len(expected) > 0:
+        expected_index = expected[0][0]
+    else:
+        expected_index = None
+    assert result == expected_index
+    
+
+def input_args_2d(data1=test_series, data2=neg_test_series, indexBegin=3, indexEnd=9, threshold1=0.5, threshold2=-4.5, winLength=2):
+    return {'data1': data1, 'data2': data2, 'indexBegin': indexBegin, 'indexEnd': indexEnd,
+            'threshold1': threshold1, 'threshold2': threshold2, 'winLength': winLength}
+
+
+@pytest.mark.parametrize('test_name,input_dict,expected', [
+    ('basic test', input_args_2d(), 8),
+    ('higher threshold', input_args_2d(threshold2=-5.1), 4),
+    ('at higher threshold', input_args_2d(threshold2=-5.0), 8),
+    ('small window', input_args_2d(winLength=1), 4),
+    ('too large window', input_args_2d(winLength=4), None),
+    ('too large window', input_args_2d(winLength=3), None),
+    ('further end index', input_args_2d(indexEnd=12), 8),
+    ('ending on part of window', input_args_2d(indexEnd=11), 8),
+    ('starting on partial window', input_args_2d(indexBegin=2), 8),
 ])
+def test_searchContinuityAboveValueTwoSignals(test_name, input_dict, expected):
+    print(test_name)
+    assert searchContinuityAboveValueTwoSignals(**input_dict) == expected
+    # all tests should work if datasets 1 and 2 are reversed
+    (input_dict['data2'], input_dict['data1']) = (input_dict['data1'], input_dict['data2'])
+    (input_dict['threshold2'], input_dict['threshold1']) = (input_dict['threshold1'],
+                                                            input_dict['threshold2'])
+    print(searchContinuityAboveValueTwoSignals(**input_dict))
+    assert searchContinuityAboveValueTwoSignals(**input_dict) == expected
+
+
+@pytest.mark.parametrize('test_name,input_dict,expected', high_and_low_threshold_test_set)
 def test_searchMultiContinuityWithinRange(test_name, input_dict, expected):
     print(test_name)
     result = searchMultiContinuityWithinRange(**input_dict)
