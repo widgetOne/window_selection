f8c5b94258a7675bddf38d00b72b88982a3be434
Brian Coulter <pittsburgh137@gmail.com>
2017-09-24 11:44:35 -0400

    cleaned up basic test of 1-threshold model
    I'm made some errors in the initial creation
    of my first test, including getting the name wrong and
    making the test series a string inside a list.
    I cleaned these up and everything works now.

--- test__window_extraction.py
+++ test__window_extraction.py
@@ -10,12 +10,12 @@ def get_swing_data():
     return batting_data
 
 
-test_series = pandas.Series(['0, 1, 1, 0, 1, 5, 1, 0, 1, 2, 0, 1, 1, 0'])
+test_series = pandas.Series([0, 1, 1, 0, 1, 5, 1, 0, 1, 2, 0, 1, 1, 0])
 default_test_input = {'data': test_series, 'indexBegin': 3, 'indexEnd': 12, 'threshold': 0.5, 'winLength': 2}
 @pytest.mark.parametrize("input_dict,expected", [
-    (default_test_input, 8),
+    (default_test_input, 4),
 ])
-def test_searchMultiContinuityWithinRange(input_dict, expected):
+def test_searchContinuityAboveValue(input_dict, expected):
     result = searchContinuityAboveValue(**input_dict)
     assert result == expected
 
